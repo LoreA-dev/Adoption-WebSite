d61e864b8f42d899eb50f97417c9d71cfeea7834
Santiago Olayo <santiagoolayojv@gmail.com>
2021-06-06 18:04:43 -0500

    Header Desktop finished

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port',process.env.PORT || 3000)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname,'public/views/home_page.html'))
+    res.sendFile(path.join(__dirname,'/public/views/home_page.html'))
 })
 
 
