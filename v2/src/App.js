bb0b9556061168fc02ecbada06c2ad12426469e9
FlavioAandres <fpareja@condorlabs.io>
2021-10-31 17:27:18 -0500

    completed pets view

--- v2/src/App.js
+++ v2/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react' 
 import Navbar from './components/Navbar/index.js'
-import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeContainerView from './containers/HomeView'
+import PetsContainerView from './containers/PetsView'
 import './App.css';
 import './constants.css'
 
@@ -15,7 +16,7 @@ function App() {
             component={HomeContainerView}
           />
           <Route exact path="/blogs"/>
-          <Route exact path="/pets"/>
+          <Route exact path="/pets" component={PetsContainerView}/>
           {/* Aqui el codigo de lo demas */}
         </div>
       </Router>
