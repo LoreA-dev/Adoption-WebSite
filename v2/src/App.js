7f47d3eed2b92ca508017ff07dd046d6369bc445
FlavioAandres <fpareja@condorlabs.io>
2021-10-31 14:46:46 -0500

    adding routes

--- v2/src/App.js
+++ v2/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react' 
 import Navbar from './components/Navbar/index.js'
 import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
-
+import HomeContainerView from './containers/HomeView'
 import './App.css';
 import './constants.css'
 
@@ -11,7 +11,9 @@ function App() {
       <Router path="/">
         <Navbar/>
         <div className="views-container">
-          <Route exact path="/home"/>
+          <Route exact path="/home"
+            component={HomeContainerView}
+          />
           <Route exact path="/blogs"/>
           <Route exact path="/pets"/>
           {/* Aqui el codigo de lo demas */}
